90b29d1fdc3b9c4ddc24500b422a39674ecf1982
francis15-0 <nnadiekwemakuo.11@gmail.com>
2025-07-19 15:21:02 -0400

    Added styles for light and dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import Switch from '@mui/material/Switch';
 import './App.css'
+import { useState } from 'react';
 
 function App() {
-  
+  const [dark, setDark] = useState(false);
+
+  const handleToggle = () => {
+    setDark(prev => {
+      const newTheme = !prev;
+      document.body.className = newTheme ? 'dark' : 'light'; // <-- update body class
+      return newTheme;
+    });
+  };
 
   return (
     <>
@@ -15,7 +24,63 @@ function App() {
 
         <div className='subHeader'>
           <p>Dark Mode</p>
-          <Switch />
+          <Switch checked={dark} onChange={handleToggle} />
+        </div>
+
+        <div className='social-box'>
+
+          <div className='box'>
+
+            <div className='box-content'> 
+              
+              <div className='username'>
+              <img src="./public/images/icon-facebook.svg" alt="facebook" />
+              <p>@nathanf</p>
+              </div>      
+              <h1>1987</h1>
+              <p className='followers'>Followers</p>
+              <p className='date'>12 Today</p>
+            </div>
+          </div>
+          <div className='box'>
+
+            <div className='box-content'> 
+              
+              <div className='username'>
+              <img src="./public/images/icon-facebook.svg" alt="facebook" />
+              <p>@nathanf</p>
+              </div>      
+              <h1>1987</h1>
+              <p>Followers</p>
+              <p>12 Today</p>
+            </div>
+          </div>
+          <div className='box'>
+
+            <div className='box-content'> 
+              
+              <div className='username'>
+              <img src="./public/images/icon-facebook.svg" alt="facebook" />
+              <p>@nathanf</p>
+              </div>      
+              <h1>1987</h1>
+              <p>Followers</p>
+              <p>12 Today</p>
+            </div>
+          </div>
+          <div className='box'>
+
+            <div className='box-content'> 
+              
+              <div className='username'>
+              <img src="./public/images/icon-facebook.svg" alt="facebook" />
+              <p>@nathanf</p>
+              </div>      
+              <h1>1987</h1>
+              <p>Followers</p>
+              <p>12 Today</p>
+            </div>
+          </div>
         </div>
 
       </div>
